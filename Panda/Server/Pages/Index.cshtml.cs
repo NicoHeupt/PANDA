11d09d60202876babddc5cecb87320f2a5529a36
Circlebit <paul.seibel@mailbox.org>
2019-09-21 22:03:58 +0200

    experiments on index page

--- Panda/Server/Pages/Index.cshtml.cs
+++ Panda/Server/Pages/Index.cshtml.cs
@@ -4,14 +4,29 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Server.Entities;
 
 namespace Server.Pages
 {
     public class IndexModel : PageModel
     {
+        private readonly IPandaRepository pandaRepo;
+
+        public IndexModel(IPandaRepository pandaRepository)
+        {
+            pandaRepo = pandaRepository;
+        }
+        
+
+        public List<Trader> Traders { get; set; }
+        public List<Product> Products { get; set; }
+        //public List<MarketProduct> Market { get; set; }
+        //public List<BookingOrder> BookingOrders { get; set; }
+
         public void OnGet()
         {
-
+            Traders = pandaRepo.GetAllTraders().ToList();
+            Products = pandaRepo.GetAllProducts().ToList();
         }
     }
 }
