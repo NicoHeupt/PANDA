f87d3424736db9584439926f2fcd087a7620c627
Circlebit <paul.seibel@gmail.com>
2019-12-10 18:01:06 +0100

    Server23 /👁\

---
+++ Panda/Server23/Data/PandaDbContext.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Server23.Entities;
+
+namespace Server23.Data
+{
+    public class PandaDbContext : IdentityDbContext
+    {
+        public PandaDbContext(DbContextOptions<PandaDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Trader> Traders { get; set; }
+        public DbSet<MarketProduct> Market { get; set; }
+        public DbSet<BookingOrder> BookingOrders { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<DepotPosition>()
+                .HasKey(dp => new { dp.ProductCode, dp.DepotId });
+
+            modelBuilder.Entity<Trader>()
+                .HasAlternateKey(t => t.Name);
+
+            modelBuilder.Entity<Trader>()
+                .HasOne(t => t.BankAccount).WithOne();
+
+            modelBuilder.Entity<Trader>()
+                .OwnsOne(t => t.Depot);
+
+            modelBuilder.Entity<BankAccount>()
+                .HasMany(ba => ba.BankTransactions).WithOne();
+
+            //modelBuilder.Entity<BankAccount>()
+            //    .HasOne(ba => ba.Trader).WithOne();
+
+            modelBuilder.Entity<BankTransaction>()
+                .HasOne(bt => bt.BankAccount).WithMany();
+
+            modelBuilder.Entity<Depot>()
+                .HasMany(ba => ba.Transactions).WithOne();
+
+            //modelBuilder.Entity<Depot>()
+            //    .HasOne(d => d.Trader).WithOne();
+        }
+    }
+}
+
---
+++ Panda/Server23/Pages/Index.cshtml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Server23.Data;
+using Server23.Entities;
+
+namespace Server23.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly IPandaRepository pandaRepo;
+
+        public IndexModel(IPandaRepository pandaRepository)
+        {
+            pandaRepo = pandaRepository;
+        }
+
+        public List<MarketProduct> Market { get; set; }
+        public List<Trader> Traders { get; set; }
+
+        //public List<BookingOrder> BookingOrders { get; set; }
+
+        public void OnGet()
+        {
+            Market = pandaRepo.GetAllMarketProducts()
+                .OrderBy(mp => mp.ProductCode)
+                .ToList();
+
+            Traders = pandaRepo.GetAllTraders()
+                .OrderBy(t => t.BankAccount.Balance)
+                .Reverse()
+                .ToList();
+        }
+    }
+}
