c3f513c9cf7ef47e55116d2f7c5a29d2837c223b
Circlebit <paul.seibel@gmail.com>
2019-12-11 19:39:46 +0100

    Erste Schritte Orders Api Controllerm Erste Schritte Booking Page

---
+++ Panda/Server23/Pages/Booking.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Server23.Data;
+using Server23.Entities;
+
+namespace Server23
+{
+    public class BookingModel : PageModel
+    {
+        private readonly IPandaRepository pandaRepo;
+
+        public BookingModel(IPandaRepository pandaRepository)
+        {
+            pandaRepo = pandaRepository;
+        }
+
+        public List<MarketProduct> Market { get; set; }
+
+        public void OnGet()
+        {
+            Market = pandaRepo.GetAllMarketProducts()
+                .OrderBy(mp => mp.ProductCode)
+                .ToList();
+        }
+    }
+}
